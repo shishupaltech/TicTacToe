db6b95c874f7f0967678dd03ecd4c2899c994580
shishupaltech <veerutech75@gmail.com>
2022-09-18 23:54:37 +0530

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import First from './components/First';
+const App = () =>{
+  return(
+    <>
+    <First/>
+  </>
+  )
+}
+export default App;
---
+++ src/components/First.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+const First = ()=>{
+    return(
+        <>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Confirmation</li>
+                </ul>
+            </nav>
+        </>
+    )
+}
+export default  First;
